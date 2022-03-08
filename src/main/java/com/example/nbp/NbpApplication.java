6605e08b32525427c6f59e6b3b486a146abbda2d
Dodzik <przemoaaa@gmail.com>
2022-03-08 19:18:18 +0100

    quick fix

--- src/main/java/com/example/nbp/NbpApplication.java
+++ src/main/java/com/example/nbp/NbpApplication.java
@@ -1,7 +1,6 @@
 package com.example.nbp;
 
 
-import com.mashape.unirest.http.exceptions.UnirestException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
